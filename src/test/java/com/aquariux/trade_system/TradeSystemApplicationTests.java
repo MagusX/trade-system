7ec4afe9303e96c2e4c5aab1282f0c342539681d
khoa.doan <khoa.doan@mservice.com.vn>
2025-03-25 01:16:45 +0700

    initial commit

---
+++ src/test/java/com/aquariux/trade_system/TradeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquariux.trade_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
