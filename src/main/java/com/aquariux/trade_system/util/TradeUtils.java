396ed6528a9110c8b133f3b977f2fd2a327aa33b
khoa.doan <khoa.doan@mservice.com.vn>
2025-03-25 19:05:03 +0700

    Add trade util

---
+++ src/main/java/com/aquariux/trade_system/util/TradeUtils.java
@@ -0,0 +1,9 @@
+package com.aquariux.trade_system.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TradeUtils {
+    public static final String SIDE_BUY = "BUY";
+    public static final String SIDE_SELL = "SELL";
+}
